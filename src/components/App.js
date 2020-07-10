23c8735a0b69795161a24012a75190d77befa46d
bryanmej <bryanmej@gmail.com>
2020-07-10 13:11:09 -0500

    added pokedex and catch button functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [wildPoke, setWildPoke] = useState({});
+  const [pokedex, setPokedex] = useState([]);
 
   useEffect(() => {
-    FindWildPoke();
+    findWildPoke();
   }, []);
 
   const pokeId = () => {
@@ -13,11 +14,19 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const FindWildPoke = async () => {
+  const findWildPoke = async () => {
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId()}`)
       .then((res) => res.json())
       .then((res) => setWildPoke(res));
     return data;
+  };
+
+  const catchPoke = (poke) => {
+    if (pokedex.indexOf(poke) < 0) {
+      const pokeArr = [...pokedex, poke];
+      setPokedex(pokeArr);
+      findWildPoke();
+    }
   };
 
   return (
@@ -26,7 +35,27 @@ const App = () => {
         <h1>Pokemon API</h1>
       </header>
 
-      <section>{wildPoke.name}</section>
+      <section>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${wildPoke.id}.png`}
+          alt={`${wildPoke.name}`}
+        />
+        <h3>{wildPoke.name}</h3>
+        <button onClick={() => catchPoke(wildPoke)}>Catch</button>
+      </section>
+
+      <section>
+        <h2>Pokedex</h2>
+        {pokedex.map((poke) => (
+          <div key={poke.id}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`}
+              alt={`${wildPoke.name}`}
+            />
+            <h3>{wildPoke.name}</h3>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
