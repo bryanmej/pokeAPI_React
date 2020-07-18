e2be51de820cfe24b21737b0c4a569d8c3472be2
bryanmej <bryanmej@gmail.com>
2020-07-18 16:29:42 -0500

    created hoc for data fetching fixed sytles in pokedex component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/Appstyles.css";
 import Pokedex from "./Pokedex";
 import WildPokemon from "./WildPokemon";
 
 const App = () => {
-  const [wildPoke, setWildPoke] = useState({});
-  const [pokedex, setPokedex] = useState([]);
-
-  useEffect(() => {
-    findWildPoke();
-  }, []);
-
-  const pokeId = () => {
-    const min = Math.ceil(1);
-    const max = Math.floor(151);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  const findWildPoke = async () => {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId()}`)
-      .then((res) => res.json())
-      .then((res) => setWildPoke(res));
-    return data;
-  };
+  const [pokedex, setPokedex] = React.useState([]);
 
   const catchPoke = (poke) => {
     if (pokedex.indexOf(poke) < 0) {
       const pokeArr = [...pokedex, poke];
       setPokedex(pokeArr);
-      findWildPoke();
     }
   };
 
@@ -43,7 +24,7 @@ const App = () => {
         <h1>Pokemon API</h1>
       </header>
 
-      <WildPokemon wildPoke={wildPoke} catchPoke={catchPoke} />
+      <WildPokemon catchPoke={catchPoke} />
       <Pokedex pokemon={pokedex} releasePoke={releasePoke} />
     </div>
   );
--- src/components/WildPokemon.js
+++ src/components/WildPokemon.js
@@ -1,25 +1,47 @@
 import React from "react";
 import "../styles/WildPokemonStyles.css";
+import withData from "./withData";
 
-const WildPokemon = ({ wildPoke, catchPoke }) => {
+const WildPoke = ({
+  catchPoke = (f) => f,
+  fetchData = (f) => f,
+  data = {},
+}) => {
   return (
     <section className="wildPokemonArea">
       <h2 className="encounter">Wild Encounter</h2>
       <img
         src={
-          wildPoke
-            ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${wildPoke.id}.png`
+          data
+            ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
             : null
         }
         className="wildPoke"
-        alt={`${wildPoke.name}`}
+        alt={`${data.name}`}
       />
-      <h3 className="wildName">{wildPoke.name}</h3>
-      <button className="catch-btn" onClick={() => catchPoke(wildPoke)}>
+      <h3 className="wildName">{data.name}</h3>
+      <button
+        className="catch-btn"
+        onClick={() => {
+          catchPoke(data);
+          fetchData(`https://pokeapi.co/api/v2/pokemon/${pokeId()}`);
+        }}
+      >
         Catch
       </button>
     </section>
   );
 };
+
+const pokeId = () => {
+  const min = Math.ceil(1);
+  const max = Math.floor(151);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const WildPokemon = withData(
+  WildPoke,
+  `https://pokeapi.co/api/v2/pokemon/${pokeId()}`
+);
 
 export default WildPokemon;
